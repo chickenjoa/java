73fdf574366a50ad958c716a96acc2e9262a8976
Taejun Park <tea_jun@naver.com>
2022-04-26 11:23:37 +0900

    main

---
+++ Tutorial11/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static int factor(int number) {
+		int sum  =1;
+		for(int i =2; i <=number; i++)
+		{
+			sum += i;
+		}
+		return sum;
+	}
+	public static void main(String[] args) {
+		System.out.println("10ÆÑÅä¸®¾óÀº"+factor(10));
+
+	}
+
+}
