ac6ea3eb3c052104d6ee5461caf01891de7f1db9
Taejun Park <tea_jun@naver.com>
2022-04-26 13:19:26 +0900

    .

---
+++ Tutorial12/src/Main2.java
@@ -0,0 +1,21 @@
+
+public class Main2 {
+	
+	public static int fibonacci(int number) {
+		if(number ==1)
+		{
+			return 1;
+		}
+		else if(number == 2)
+		{
+			return 1;
+		}
+		else
+			return fibonacci(number -1) + fibonacci(number - 2);
+	}
+	public static void main(String[] args) {
+		System.out.println("피보나치 수열의 -1번째 원소는" + fibonacci(-1) + "입니다.");
+
+	}
+
+}
